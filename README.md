495bea22e7a2f7b0405b12b3515ae514772c1a41
Yanxix <infielderdia@hotmail.com>
2024-09-17 15:15:48 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,54 @@
+﻿conda create -n NestedUnetTorch python=3.6
+
+conda activate NestedUnetTorch
+
+pip install simpleitk
+
+pip install opencv-python==3.4.2.16
+
+pip install scipy
+
+pip install scikit-learn==0.20
+
+pip install scikit-image
+
+conda  install numpy  mkl cffi
+
+conda install  torchvision  -c pytorch
+
+conda install Pillow=6.1
+
+conda install tqdm
+
+conda install pandas
+
+这里提供一个数据集，Unet++官方提供的
+1. Download dataset from [here](https://www.kaggle.com/c/data-science-bowl-2018/data) to inputs/ and unzip. The file structure is the following:
+```
+inputs
+└── <yourdataname>
+ ├── images
+     └── 00ae65...
+ └── masks
+     └── 00ae65...            
+    ...
+```
+
+
+## Results
+### DSB2018 (96x96)
+
+Here is the results on DSB2018 dataset (96x96) with LovaszHingeLoss.
+
+| Model                           |   IoU   |  Loss   |
+|:------------------------------- |:-------:|:-------:|
+| U-Net                           |  0.839  |  0.365  |
+| Nested U-Net                    |  0.842  |**0.354**|
+| Nested U-Net w/ Deepsupervision |**0.843**|  0.362  |
+
+
+
+运行代码：
+python train.py --dataset <dataset name> --arch NestedUNet --image-ext png --mask-ext png
+
+根据命令行函数自己加参数就行
---
+++ dataset.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2 #https://www.jianshu.com/p/f2e88197e81d
+import random
+
+from skimage.io import imread
+from skimage import color
+
+import torch
+import torch.utils.data
+from torchvision import datasets, models, transforms
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, args, img_paths, mask_paths, aug=False):
+        self.args = args
+        self.img_paths = img_paths
+        self.mask_paths = mask_paths
+        self.aug = aug
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.img_paths[idx]
+        mask_path = self.mask_paths[idx]
+        image = imread(img_path)
+        mask = imread(mask_path)
+
+        image = image.astype('float32') / 255
+        mask = mask.astype('float32') / 255
+
+        if self.aug:
+            if random.uniform(0, 1) > 0.5:
+                image = image[:, ::-1, :].copy()
+                mask = mask[:, ::-1].copy()
+            if random.uniform(0, 1) > 0.5:
+                image = image[::-1, :, :].copy()
+                mask = mask[::-1, :].copy()
+
+        image = color.gray2rgb(image)
+        #image = image[:,:,np.newaxis]
+        image = image.transpose((2, 0, 1))
+        mask = mask[:,:,np.newaxis]
+        mask = mask.transpose((2, 0, 1))
+
+        return image, mask
